9a9a12de70a7b9757cceb256fa3eacd8d437c86a
leeping <leeping@APING>
2025-07-12 09:19:36 +0800

    Add project files.

---
+++ web_assignment/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using web_assignment.Models;
+
+namespace web_assignment.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account/Login
+
+        public IActionResult Login()
+        {
+            LoginViewModel viewModel = new LoginViewModel();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            return RedirectToAction("test");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
